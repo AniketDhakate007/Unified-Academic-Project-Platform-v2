4d33cb9e8d65b2b5a8207af81012cce7cb8108ac
AniketDhakate007 <adhakate344@gmail.com>
2025-08-09 00:05:19 +0530

    Initial commit

---
+++ ProjectApplication/src/test/java/com/UAPP/ProjectApplication/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UAPP.ProjectApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
